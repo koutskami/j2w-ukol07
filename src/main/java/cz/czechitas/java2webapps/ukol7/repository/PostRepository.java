3740f4c2a3922a22354ff3244b3a8d453df2c40d
koutskami <koutskami@gmail.com>
2024-06-22 14:45:27 +0200

    nachystano po bod 9

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package cz.czechitas.java2webapps.ukol7.repository;
+
+import cz.czechitas.java2webapps.ukol7.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findOne(String slug);
+}
---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/services/PostService.java
@@ -0,0 +1,25 @@
+package cz.czechitas.java2webapps.ukol7.services;
+
+import cz.czechitas.java2webapps.ukol7.entity.Post;
+import cz.czechitas.java2webapps.ukol7.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private final PostRepository postRepository;
+
+    @Autowired
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
+    public Optional<Post> singlePost(String slug) {
+        return postRepository.findOne(slug);
+    }
+}
